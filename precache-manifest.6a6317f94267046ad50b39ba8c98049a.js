13971fe71fce94b469f6332be02d38482b240334
godkun <godkun666@126.com>
2023-12-17 15:18:35 +0800

    feat: website

---
+++ precache-manifest.6a6317f94267046ad50b39ba8c98049a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53126133c2bec218af06",
+    "url": "./static/css/main.1f3fd1da.chunk.css"
+  },
+  {
+    "revision": "53126133c2bec218af06",
+    "url": "./static/js/main.53126133.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "84e6cf5bdc8e78d4f84f",
+    "url": "./static/css/2.2a3fdab0.chunk.css"
+  },
+  {
+    "revision": "84e6cf5bdc8e78d4f84f",
+    "url": "./static/js/2.84e6cf5b.chunk.js"
+  },
+  {
+    "revision": "1db21e00c2dcf79b62c1d3d5493cd382",
+    "url": "./index.html"
+  }
+];
